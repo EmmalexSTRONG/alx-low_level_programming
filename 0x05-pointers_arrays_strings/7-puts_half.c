de927741961f83da3a343fafc22fd53b6a374327
EmmalexSTRONG <gokahemma1999@gmail.com>
2023-03-27 20:18:17 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,16 @@
  */
 
 void puts_half(char *str)
-
 {
-        int j = 0;
-        int k;
+	int j = 0;
+	int k;
 
-        while (str[j] != '\0')
-        {
-        j++;
-        }
-        if (j % 2 == 1)
-        {
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+	if (j % 2 == 1)
+	{
 	k = (j - 1) / 2;
 	k += 1;
 	}
@@ -29,7 +28,7 @@ void puts_half(char *str)
 	for (; k < j; k++)
 	{
 	_putchar(str[k]);
-	}	
+	}
 	_putchar('\n');
 }
 
